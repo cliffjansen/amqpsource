867fe633cf278b867c9a0eed880c3a06879bffb6
Cliff Jansen <cjansen@redhat.com>
2018-12-17 10:00:33 -0800

    handle case of single data section with application/json content

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # amqpsource
 AMQP cloudevent source for knative eventing
+
+## Work in Progress
+
+Currently the source can only handle AMQP messages in a very particular format
+due to limitations in the github.com/knative/pkg/cloudevents package:
+
+The message must have content_type property "application/json" and a single Data
+section that when converted to a string is valid input to
+[json.Marshal()](https://golang.org/pkg/encoding/json/#Marshal).
--- pkg/adapter/amqpsource/adapter.go
+++ pkg/adapter/amqpsource/adapter.go
@@ -22,6 +22,7 @@ import (
 	"github.com/knative/pkg/logging"
 	"go.uber.org/zap"
 	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"log"
@@ -39,6 +40,12 @@ import (
 	"qpid.apache.org/electron"
 )
 
+type amqpBodyReader struct {
+	body string
+	offset int
+}
+
+
 type Adapter struct {
 	// URI-eske connection and address info to attach to the AMQP endpoint
 	// (confusingly also a "source") via AMQP or AMQPS protocol.
@@ -50,6 +57,8 @@ type Adapter struct {
 	InsecureTlsConnection bool
 	// Only needed if using TLS and default root CAs in container do not suffice.
 	RootCA string
+	// The canonical name for the CloudEvents "source" Context Attribute.
+	SpecSource string
 }
 
 var msgCount = int64(0)
@@ -61,13 +70,14 @@ func (a *Adapter) Start() error {
 	// ZZZ ??? do we fail fast if problems connecting/receiving or do our own backoff retry loop?
 	// set up signals so we handle the first shutdown signal gracefully
 
-	log.Printf("Start with : %s", a.SourceURI)
-	// ZZZ getpid not unique enough...
-	container := electron.NewContainer(fmt.Sprintf("amqp_event_source_002_[%v]", os.Getpid()))
+	log.Printf("Start with ZZZ1  : %s and %s", a.SourceURI, os.Getenv("HOSTNAME"))
+	// Use Kubernetes PODNAME-uuid as descriptive and unique AMQP container name:
+	container := electron.NewContainer(fmt.Sprintf("%s", os.Getenv("HOSTNAME")))
 	u, err := amqp.ParseURL(a.SourceURI)
 	fatalIf(err)
+	a.SpecSource = fmt.Sprintf("%s://%s:%s/%s", u.Scheme, u.Hostname(), u.Port(), u.Path)
 	log.Printf("Dial")
-	tcpconn, err := a.dial(u)// ZZZ container.Dial("tcp", u.Host) // NOTE: Dial takes just the Host part of the URL
+	tcpconn, err := a.dial(u)
 	fatalIf(err)
 	amqpconn, err := container.Connection(tcpconn)
 	fatalIf(err)
@@ -96,7 +106,7 @@ func (a *Adapter) Start() error {
 			fatalIf(err)
 		}
 	}
-	//amqpconn.Close(nil)  where does this go? ZZZ
+	//amqpconn.Close(nil)  defer... or irrelevant?
 	log.Printf("NOTREACHED reached")
 	return nil
 }
@@ -104,14 +114,47 @@ func (a *Adapter) Start() error {
 func (a *Adapter) postMessage(m *amqp.Message) error {
 	logger := logging.FromContext(context.TODO())
 
+	// TODO: check for existing CloudEvents headers to see if we are just forwarding an existing event.
+	// The following code creates a new CloudEvents event from an arbitrary AMQP message.
+
+	var rdr *amqpBodyReader
+	ctype := (*m).ContentType()
+	var body = (*m).Body()
+	log.Printf("body switch time with : %T ... %v", body, body)
+	switch body.(type) {
+	case string:
+		log.Printf("body switch string")
+		ctype = "text/plain; charset=utf-8"
+		rdr = newAmqpBodyReader(body.(string))
+	case amqp.Binary:
+		log.Printf("body switch bin")
+		if ctype == "" {
+			ctype = "application/octet-stream"
+			rdr = newAmqpBodyReader(body.(amqp.Binary).String())
+		}
+	default:
+		log.Printf("body switch whoops")
+		ctype = "application/ZZZ"
+		rdr = newAmqpBodyReader("ZZZ and ZZZ")
+	}
+
 	ctx := cloudevents.EventContext{
 		CloudEventsVersion: cloudevents.CloudEventsVersion,
 		EventType:          "amqp.message.delivery",
 		EventID:            messageIdString(m),
 		EventTime:          (*m).CreationTime(),
-		Source:             "some_canon_amqpaddr_rep_TODO", // Expose no secrets
+		Source:             a.SpecSource,
+		ContentType:        ctype,
 	}
-	req, err := cloudevents.Binary.NewRequest(a.SinkURI, m, ctx)
+	var err error
+	var req *http.Request
+	if rdr != nil {
+		req, err = cloudevents.Binary.NewRequest(a.SinkURI, rdr, ctx)
+	} else {
+		// Binary data section in json.
+		// For now, lib just calls json.Marshall(), so pass as string, not reader
+		req, err = cloudevents.Binary.NewRequest(a.SinkURI, body.(amqp.Binary).String(), ctx)
+	}
 	if err != nil {
 		log.Printf("Failed to marshal the message: %+v : %s", m, err)
 		return err
@@ -125,8 +168,8 @@ func (a *Adapter) postMessage(m *amqp.Message) error {
 		return err
 	}
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	logger.Debug("response", zap.Any("status", resp.Status), zap.Any("body", string(body)))
+	respbody, _ := ioutil.ReadAll(resp.Body)
+	logger.Debug("response", zap.Any("status", resp.Status), zap.Any("body", string(respbody)))
 	return nil
 }
 
@@ -137,7 +180,6 @@ func (a *Adapter) dial(u *url.URL) (conn net.Conn, err error) {
 	var roots *x509.CertPool = nil
 	if a.RootCA != "" {
 		roots = x509.NewCertPool()    // override container's root CAs
-		log.Println("ZZZ1")
 		ok := roots.AppendCertsFromPEM([]byte(a.RootCA))
 		if !ok {
 			err = fmt.Errorf("adapter.dial: bad Root CA encoding") // Any other possible reason?
@@ -179,4 +221,25 @@ func messageIdString(m *amqp.Message) string {
 		return ""
 	}
 
+}
+
+func newAmqpBodyReader (b string) *amqpBodyReader {
+	log.Printf("body reader NEW with : %v", b)
+	return &amqpBodyReader{
+		body:    b,
+		offset:  0,
+	}
+}
+
+func (rdr *amqpBodyReader) Read(out []byte) (n int, err error) {
+	if rdr.offset < len(rdr.body) {
+		bytes := rdr.body[rdr.offset:]
+		n = copy(out, bytes)
+		rdr.offset += n
+	}
+	if rdr.offset >= len(rdr.body) {
+		err = io.EOF
+	}
+	log.Printf("body reader READ with : %d and %v", n, err)
+	return
 }
